59ba47ad3892a7c66a1b26a4e8746b8b07f00fef
Zhang-Xinyue12 <zhangxinyue_1212@163.com>
2025-01-14 10:36:45 +0100

    Update homework9.py

--- homework9.py
+++ homework9.py
@@ -17,7 +17,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"You said: {user_message}")
 if __name__ == '__main__':
     # Create an application instance and pass in API token
-    application = ApplicationBuilder().token('7830105491:AAF8t7FlaxWNykNPMdUN3KsdRr3_Vdh0fKM').build()
+    application = ApplicationBuilder().token('Your Robot API Token').build()
     # Adding a Command Processor and Message Processor
     application.add_handler(CommandHandler('start', start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
